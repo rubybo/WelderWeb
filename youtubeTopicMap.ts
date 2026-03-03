d0c4b3197fb3cc38f9d99069440c68741c8fbf75
Aleksey <kapturalex4@gmail.com>
2026-03-03 13:46:07 +0300

    Add YouTube video mapping for topics

--- youtubeTopicMap.ts
+++ youtubeTopicMap.ts
@@ -4,4 +4,18 @@
 export const YOUTUBE_PLAYLIST_ID = "PL5qLa3XgKTh6A92exFo_bYHOwDh7GDbyH";
 
 export const YOUTUBE_TOPIC_TO_VIDEO_ID: Record<number, string> = {
+  1: "p0ALZivBBxg",
+  2: "gDIDkmIP8dI",
+  3: "8V4satIRbeU",
+  4: "CTScJRwSFbE",
+  5: "0ldphx4DkWU",
+  6: "LHV4oNyk_Jk",
+  7: "QmtzfEkmO0Q",
+  8: "SPBiK_-MOxw",
+  9: "g_eyNnQYU_k",
+  12: "UF5lqJhx_ow",
+  13: "N_DJ0Tf6EtA",
+  14: "T-xJKPxidXY",
+  15: "U1sVig_zcSQ",
+  16: "xcz_eZpIb6g",
 };
